c5f498e8477e1a05675e58bef1eae72928f9020d
163114 <88505337+163114@users.noreply.github.com>
2022-02-14 17:43:15 +0300

    Add files via upload

---
+++ 15 day/src/main_executable_module/main_executable_module.c
@@ -0,0 +1,27 @@
+#include "data_io.h"
+#include "data_stat.h"
+
+void main()
+{
+    double *data;
+    int n;
+
+    printf("LOAD DATA...\n");
+    input(data, n);
+
+    printf("RAW DATA:\n\t");
+    output(data, n);
+
+    printf("\nNORMALIZED DATA:\n\t");
+    normalization(data, n);
+    output(data, n);
+    
+    printf("\nSORTED NORMALIZED DATA:\n\t");
+    sort(data, n);
+    output(data, n);
+    
+    printf("\nFINAL DECISION:\n\t");
+    //make_decision(data, n);
+    //...
+    
+}
