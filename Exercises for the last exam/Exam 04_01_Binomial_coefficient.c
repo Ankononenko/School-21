b6169ee22cfdbee0c4a5da68efd4a63722138bc9
163114 <88505337+163114@users.noreply.github.com>
2022-02-25 23:51:02 +0300

    Create Exam 04_01_Binomial_coefficient.c

---
+++ Exercises for the last exam/Exam 04_01_Binomial_coefficient.c
@@ -0,0 +1,31 @@
+// Copyright 2022 finchren
+
+#include <stdio.h>
+
+int factorial(int number);
+int c_formula(int number, int k);
+
+int main() {
+    int number;
+    scanf("%d", &number);
+    for (int k = 0; k <= number; k++) {
+        printf("%d", c_formula(number, k));
+        if (k != number) {
+            printf(" ");
+        }
+    }
+    return 0;
+}
+
+int factorial(int number) {
+    int number_to_return = 1;
+    for (int i = 0; i <= number; i++) {
+        number_to_return *= i;
+    }
+    return number_to_return;
+}
+
+int c_formula(int number, int k) {
+    int result = factorial(number) / (factorial(k) * factorial(number - k));
+    return result;
+}
