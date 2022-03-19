dafe80f25afd53b15b32d7111df2971563c83fe7
163114 <88505337+163114@users.noreply.github.com>
2022-03-19 20:27:16 +0300

    Update 33. Pascal's triangle.c
    Solved

--- Exercises for the last exam/Random exercises/For loop/33. Pascal's triangle.c
+++ Exercises for the last exam/Random exercises/For loop/33. Pascal's triangle.c
@@ -19,12 +19,12 @@ Expected Output :
 + 2. Take input
 + 3. Write factorial function
 + 3.5 Function to count the formula
-4. Write funciton to display the pyramid
-5. Spaces inside the pyramid
-6. Invalid input
-7. Test
-8. Cpplint test
-9. Add and push
++ 4. Write funciton to display the pyramid
++ 5. Spaces inside the pyramid
++ 6. Invalid input
++ 7. Test
++ 8. Cpplint test
++ 9. Add and push
 */
 
 #include <stdio.h>
@@ -32,15 +32,47 @@ Expected Output :
 int input_rows();
 int factorial(int number);
 int n_choose_k(int n, int k);
+void print_pyramid(int rows);
+void print_spaces(int number_of_spaces);
+void print_invalid_input();
 
 int main() {
   int number_of_rows = input_rows();
-  
+  if (number_of_rows > 0) {
+    print_pyramid(number_of_rows);
+  } else {
+    print_invalid_input();
+  }
   return 0;
 }
 
+void print_invalid_input() {
+  printf("n/a");
+}
+
+void print_spaces(int number_of_spaces) {
+  for (int i = 1; i <= number_of_spaces; i++) {
+    printf(" ");
+  }
+}
+
+void print_pyramid(int rows) {
+  int counter_for_rows = 1;
+  // First for loop in the row of the pyramid
+  for (int n = 0; n < rows; n++) {
+    print_spaces(rows - counter_for_rows);
+    // Second for loop in the column of the pyramid
+    for (int k = 0; k < counter_for_rows; k++) {
+      printf("%d ", n_choose_k(n, k));
+    }
+    counter_for_rows++;
+    printf("\n");
+  }
+}
+
 int n_choose_k(int n, int k) {
-  int result = factorial(n) / factorial(k) * factorial((n - k));
+  int result = 0;
+  result = factorial(n) / (factorial(k) * factorial((n - k)));
   return result;
 }
 
@@ -54,6 +86,9 @@ int factorial(int number) {
 
 int input_rows() {
   int number_of_rows;
-  scanf("%d", &number_of_rows);
+  char endline;
+  if (!scanf("%d%c", &number_of_rows, &endline) || endline != '\n' || number_of_rows < 1) {
+    number_of_rows = 0;
+  }
   return number_of_rows;
 }
