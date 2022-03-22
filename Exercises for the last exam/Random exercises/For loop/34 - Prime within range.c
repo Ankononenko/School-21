f7ef96f0e49155882303c17d649d4681ea36d05e
163114 <88505337+163114@users.noreply.github.com>
2022-03-22 23:54:17 +0300

    Started a new exercise after a few days break

---
+++ Exercises for the last exam/Random exercises/For loop/34 - Prime within range.c
@@ -0,0 +1,24 @@
+// Copyright 2022 finchren
+/*
+Write a program in C to find the prime numbers within a range of numbers
+Test Data :
+Input starting number of range: 1
+Input ending number of range : 50
+Expected Output :
+The prime number between 1 and 50 are :
+2 3 5 7 11 13 17 19 23 29 31 37 41 43 47
+
+1. Take input
+2. Write a function to find prime in range
+3. Display output
+4. Invalid input
+5. Test
+6. Cpplint test
+7. Add and push
+*/
+
+#include <stdio.h>
+
+int main() {
+  return 0;
+}
