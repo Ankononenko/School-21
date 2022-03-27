577336ec30756faada84f2a328be3373c2f3c16e
163114 <88505337+163114@users.noreply.github.com>
2022-03-27 13:12:40 +0300

    Update 39. In range divisible by.c

--- Exercises for the last exam/Random exercises/For loop/39. In range divisible by.c
+++ Exercises for the last exam/Random exercises/For loop/39. In range divisible by.c
@@ -1,6 +1,6 @@
 // Copyright 2022 ficnhren
 /*
-Write a program in C to find the number and sum of all integer between 100 and 200 which are divisible by 9. Go to the editor
+Write a program in C to find the number and sum of all integer between 100 and 200 which are divisible by 9
 Expected Output :
 Numbers between 100 and 200, divisible by 9 :
 108 117 126 135 144 153 162 171 180 189 198
