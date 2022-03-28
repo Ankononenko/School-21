ff6db6bad4bafae38819ee5c9c978c78f4becd96
163114 <88505337+163114@users.noreply.github.com>
2022-03-28 20:09:14 +0300

    Create 41. Decimal to binary.c

---
+++ Exercises for the last exam/Random exercises/For loop/41. Decimal to binary.c
@@ -0,0 +1,21 @@
+// Copyright 2022 finchren
+/*
+Write a program in C to convert a decimal number into binary without using an array
+Test Data :
+Input a decimal number: 25
+Binary number equivalent to said decimal number is: 0000000000000000000000000001 1001
+
+1. Take the input of decimal number
+2. Figure out how to convert decimal to binary
+2. Write the function to convert and print out the result
+3. Work around invalid input
+4. Test
+5. Cpplint test
+6. Add and push
+*/
+
+#include <stdio.h>
+
+int main() {
+    return 0;
+}
