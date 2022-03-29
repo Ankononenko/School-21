4496fec50544459e6a3bf059bb255668321aa00f
163114 <88505337+163114@users.noreply.github.com>
2022-03-29 23:54:03 +0300

    Finished

--- Test tasks for job positions/Mike - Tests for an array.c
+++ Test tasks for job positions/Mike - Tests for an array.c
@@ -9,73 +9,69 @@
 + 2. Write a fucntion to find pre-max
 + 3. Multiply
 + 4. Output the result
-5. Test
-6. Cpplint test
++ Decompose the code
++ 5. Write the test cases
++ 6. Cpplint test
 7. Add and push
 */
 
 #include <stdio.h>
 
-void test();
-int get_product(int *array, int array_size);
+void test(int array[], int expected, int array_size, int test_number);
+int get_product(int array[], int array_size);
 
 int main() {
-    test();
+    // Test for values in the middle of the array
+    int array_1[10] = {1, 0, 0, 4, 5, 60, 7, 0, 0, 0};
+    test(array_1, 420, 10, 1);
+    // Test for values on the other sides of the array
+    int array_2[10] = {70, 0, 0, 4, 5 , 60, 7, 0, 0, 80};
+    test(array_2, 5600, 10, 2);
+    // Duplicate value test
+    int array_3[10] = {70, 70, 70, 70, 5 , 60, 80, 80, 80, 80};
+    test(array_3, 5600, 10, 3);
+    // Zeros and duplicate values
+    int array_4[10] = {0, 0, 0, 0, 60, 60, 0, 10, 80, 80};
+    test(array_4, 4800, 10, 4);
+    // Failed test from the call with Mike
+    int array_5[9] = {70, 0, 0 , 5, 5, 7, 7, 0, 10};
+    test(array_5, 700, 9, 5);
     return 0;
 }
 
-void test() {
-    int array_1[10] = {1, 0, 0, 4 ,5, 60, 7, 0, 0, 0};
-    int array_size_1 = (int)sizeof(array_1) / 4;
-
-    int expected_1 = 420;
-    int actual_1 = get_product(array_1, array_size_1);
-    if (expected_1 == actual_1) {
-        printf("Success");
+void test(int array[], int expected, int array_size, int test_number) {
+    int actual = get_product(array, array_size);
+    if (expected == actual) {
+        printf("\nSuccess");
+        printf("\nTest number %d: Expected = %d, actual = %d", test_number, expected, actual);
     } else {
-        printf("Failed");
-        printf("Expected = %d, actual = %d\n", expected_1, actual_1);
-    }
-    printf("\n");
-    
-    int array_2[9] = {70, 0, 0, 5 ,5, 7, 7, 0, 100};
-    int array_size_2 = (int)sizeof(array_2) / 4;
-
-    int expected_2 = 7000;
-    int actual_2 = get_product(array_2, array_size_2);
-    if (expected_2 == actual_2) {
-        printf("Success");
-    } else {
-        printf("Failed");
-        printf("Expected = %d, actual = %d\n", expected_2, actual_2);
-    }
-    printf("\n");
-
-    int array_3[9] = {70, 0, 0, 5 ,5, 7, 7, 0, 10};
-    int array_size_3 = (int)sizeof(array_3) / 4;
-
-    int expected_3 = 700;
-    int actual_3 = get_product(array_3, array_size_3);
-    if (expected_3 == actual_3) {
-        printf("Success");
-    } else {
-        printf("Failed\n");
-        printf("Expected = %d, actual = %d\n", expected_3, actual_3);
+        printf("\nFailed");
+        printf("\nTest number %d: Expected = %d, actual = %d", test_number, expected, actual);
     }
     printf("\n");
 }
 
-int get_product(int *array, int array_size) {
-    int max = array[0], premax = array[0];
-    for (int i = 0; i <= array_size - 1; ++i) {
+int get_product(int array[], int array_size) {
+    int max = array[0], min = array[0];
+    // For loop to find maximum value
+    for (int i = 0; i < array_size; ++i) {
             if (array[i] > max) {
                 max = array[i];
             }
     }
-    for (int j = 0; j <= array_size - 1; ++j) {
-        if (array[j] < max && premax < array[j]) {
+    // For loop to find minimum value to have a value for premax staring point
+    for (int i = 0; i < array_size; ++i) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+    }
+    int premax = min;
+    // For loop to find premaximum value
+    for (int j = 0; j < array_size; ++j) {
+        if (array[j] < max && array[j] > premax) {
             premax = array[j];
         }
     }
+    printf("max - %d, min - %d, premax - %d", max, min, premax);
     return (max * premax);
 }
