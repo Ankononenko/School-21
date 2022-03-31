0fc2caa1d3107056a3feb8b4f09937adf873423f
163114 <88505337+163114@users.noreply.github.com>
2022-03-31 15:19:38 +0300

    Fixed some bugs and completed the task
    Fixed bug with negative numbers and now the code converts all the numbers up to max limit of int type

--- Exercises for the last exam/Random exercises/For loop/41. Decimal to binary.c
+++ Exercises for the last exam/Random exercises/For loop/41. Decimal to binary.c
@@ -3,7 +3,10 @@
 Write a program in C to convert a decimal number into binary without using an array
 Test Data :
 Input a decimal number: 25
-Binary number equivalent to said decimal number is: 0000000000011001
+Binary number equivalent to said decimal number is: 11001
+
+Input a decimal number: -25
+Binary number equivalent to said decimal number is: -11001
 
 + 1. Take the input of decimal number
 + 2. Figure out how to convert decimal to binary: 
@@ -11,18 +14,15 @@ Binary number equivalent to said decimal number is: 0000000000011001
 26 == 1  1  0  1  0 (16 + 8 + 2 == 26)
 25 -> 16 08 04 02 01
 25 == 1  1  0  0  1 (16 + 8 + 1 == 25)
-^ That is regular binary. Now we need to figure out how to use 2's complement
-Turns out it's just 16 places for numbers, where empty spaces are filled with zeros.
-Ex: 26 == 0 0 0 0 0 0 0 0 0 0 0 1 1 0 1 0
 32768 16384 8192 4096 2048  1024 512 256 128 64 32 16 8 4 2 1
 + 2. Write the function to convert number to binary
-To do that we write a for loop where the number is being devided by the numbers of 1*2 until i <= 16
-+ 2.2 Convert it to 16-digit binary
 + 2.6 Print out the result
 + 3. Work around invalid input
-+ 3.5 Decompose main a little bit for testing
-4. Test
-5. Cpplint test
++ 3.5 Decompose main a little bit
++ 4. Test:
++ 4.1 Fix the mistake where the max input number allowed
++ 4.2 Fix negative numbers
++ 5. Cpplint test
 6. Add and push
 */
 
@@ -32,29 +32,60 @@ To do that we write a for loop where the number is being devided by the numbers 
 
 // Double input number is used to get rid of invalid input
 double input_number();
-void convert_to_binary(int number);
+void convert_to_binary(int number, int division_by_two, int is_negative);
 void print_invalid_input();
-void run_convert_to_binary(int number, double number_remainder);
-void test_binary(int number, char expected[]);
+void run_convert_to_binary(int number, double number_remainder, int division_by_two, int is_negative);
+int division_by_two(int number);
+int power(int bast, int exponent);
+int negative_number_convert(int number);
+int is_negative_check(int number);
 
 int main() {
     double number = input_number();
+    int is_negative = is_negative_check(number);
+    // Work around negative number
+    number = negative_number_convert(number);
     // Value for invalid input
     double number_remainder = remainder(number, 1);
-    run_convert_to_binary(number, number_remainder);
-    // Tests to run will be here
-
+    int division_by_two_result = division_by_two(number);
+    run_convert_to_binary(number, number_remainder, division_by_two_result, is_negative);
     return 0;
 }
 
-// void test_binary(int number, char expected[]) {
-//     double test_number_remainder = remainder(number, 1);
-//     char current[] = run_convert_to_binary(number, test_number_remainder);
-// }
+int is_negative_check(int number) {
+    int is_negative = 0;
+    if (number < 0) {
+        is_negative = 1;
+    }
+    return is_negative;
+}
 
-void run_convert_to_binary(int number, double number_remainder) {
-    if (number_remainder == 0) { 
-        convert_to_binary(number);
+int negative_number_convert(int number) {
+    if (number < 0) {
+        number *= -1;
+    }
+    return number;
+}
+
+int power(int base, int exponent) {
+    int result = base;
+    for (int i = 2; i <= exponent; ++i) {
+        result *= base;
+    }
+    return result;
+}
+
+int division_by_two(int number) {
+    int division_by_two = 0;
+    for (int i = number; i > 0; i /= 2) {
+        ++division_by_two;
+    }
+    return division_by_two;
+}
+
+void run_convert_to_binary(int number, double number_remainder, int division_by_two, int is_negative) {
+    if (number_remainder == 0) {
+        convert_to_binary(number, division_by_two, is_negative);
     } else {
         print_invalid_input();
     }
@@ -64,19 +95,28 @@ void print_invalid_input() {
     printf("n/a");
 }
 
-void convert_to_binary(int number) {
-    int divisor = 32768, result;
+void convert_to_binary(int number, int division_by_two, int is_negative) {
+    int base = 2, exponent = division_by_two - 1;
     printf("Binary number equivalent to said decimal number is:\n");
-    for (int i = 1; i <= 16; i++) {
-        result = number / divisor;
-        if (result >= 1) {
+    // Work around negative input
+    if (is_negative) {
+        printf("-");
+        }
+    // Binary output
+    for (int i = 1; i < division_by_two; ++i) {
+        if (number / power(base, exponent)) {
             printf("1");
-            number -= divisor * result;
-            divisor /= 2;
+            number -= power(base, exponent);
         } else {
             printf("0");
-            divisor /= 2;
         }
+        --exponent;
+    }
+    // If case to work with the last number in the series. For loop goes through powers of two
+    if (number == 1) {
+        printf("1");
+    } else {
+        printf("0");
     }
 }
 
