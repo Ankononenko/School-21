745cc003a64b14b2cc30b9a59ee460b0f6fa202a
163114 <88505337+163114@users.noreply.github.com>
2022-04-01 00:03:09 +0300

    Finished with tests completed the task

--- Exercises for the last exam/Random exercises/For loop/43. Highest common factor of two numbers.c
+++ Exercises for the last exam/Random exercises/For loop/43. Highest common factor of two numbers.c
@@ -11,14 +11,14 @@ HCF of 24 and 28 is : 4
 + 2. Find the HCF
 + 3. Print it out
 + 3.5 Invalid input
-4. Test
-5. Cpplint test
++ 4. Test
++ 5. Cpplint test
 6. Add and push
 */
 
 #include <stdio.h>
 
-int input_first(); 
+int input_first();
 int input_second();
 int find_hcf(int first, int second);
 void print_invalid_input();
@@ -47,19 +47,23 @@ int main() {
     test(35, 7, 7, 3);
     // Test 4 - normal value
     test(1, 35, 1, 4);
-    // Test 5 - normal value - zero to return
+    // Test 5 - normal value - zero to retur
     test(0, 99, 0, 5);
+    // Test 6 - prime number test
+    test(127, 251, 1, 6);
+    // Test 7 - double test
+    test(0.5, 9.9, 0, 7);
     return 0;
 }
 
 void test(int first, int second, int hcf_expected, int test_num) {
     int hcf_actual = find_hcf(first, second);
     if (hcf_actual == hcf_expected) {
-        printf("Test #%d: first = %d, second = %d, hcf_expected = %d, hcf_actual = %d\n", 
+        printf("Test #%d: first = %d, second = %d, hcf_expected = %d, hcf_actual = %d\n",
         test_num, first, second, hcf_expected, hcf_actual);
         printf("Test passed\n");
     } else {
-        printf("Test #%d: first = %d, second = %d, hcf_expected = %d, hcf_actual = %d\n", 
+        printf("Test #%d: first = %d, second = %d, hcf_expected = %d, hcf_actual = %d\n",
         test_num, first, second, hcf_expected, hcf_actual);
         printf("Test Failed\n");
     }
@@ -88,7 +92,7 @@ int input_first() {
     int first;
     char endline;
     printf("Input 1st number for HCF:\n");
-    if(!scanf("%d%c", &first, &endline) || first < 1 || endline != '\n') {
+    if (!scanf("%d%c", &first, &endline) || first < 1 || endline != '\n') {
         first = 0;
     }
     return first;
@@ -97,7 +101,7 @@ int input_second() {
     int second;
     char endline;
     printf("Input 2nd number for HCF:\n");
-    if(!scanf("%d%c", &second, &endline) || second < 1 || endline != '\n') {
+    if (!scanf("%d%c", &second, &endline) || second < 1 || endline != '\n') {
         second = 0;
     }
     return second;
